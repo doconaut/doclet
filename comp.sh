16c2fed8684d6ed0bf1a85fe8b4423302936cc6a
Hannes Dorfmann <hannes@tickaroo.com>
2014-06-15 18:39:58 +0200

    -docletpath option to specify path to compiled custom jar

--- comp.sh
+++ comp.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 mvn install
- build/com/doconaut/doclet/MarkdownDoclet.class
-javadoc -doclet com.doconaut.doclet.MarkdownDoclet -sourcepath src/main/java/ -docletpath build/ -private -subpackages com -template /Users/hannes/jekyll/hmfaysal-omega-theme/_doconaut/javadoc.md
+javadoc -doclet com.doconaut.doclet.MarkdownDoclet -docletpath target/doclet-1.0-SNAPSHOT.jar -sourcepath src/main/java/ -docletpath build/ -private -subpackages com -template /Users/hannes/jekyll/hmfaysal-omega-theme/_doconaut/javadoc.md
